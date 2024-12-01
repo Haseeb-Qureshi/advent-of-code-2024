53ea831b16fa38eb4fe93bf23425ebc95c84217f
Haseeb Qureshi <haseebqureshi@Haseebs-MacBook-Air.local>
2024-12-01 06:39:54 -0800

    Finish day 1

--- day01.rb
+++ day01.rb
@@ -6,3 +6,7 @@ left_list = input.map(&:first).sort!
 right_list = input.map(&:last).sort!
 
 puts left_list.zip(right_list).map { |l, r| (l - r).abs }.sum
+
+# Part 2
+counts = right_list.tally
+puts left_list.map { |num| (counts[num] || 0) * num }.sum
