bd15720b294ee887fcf88e0187a0891af104e33c
Haseeb Qureshi <haseebqureshi@Haseebs-MacBook-Air.local>
2024-12-02 21:21:01 -0800

    Finish day 3 part 2

--- day03.rb
+++ day03.rb
@@ -4,3 +4,21 @@ input = File.read('data03.txt').chomp
 
 muls = input.scan(/mul\(\d+,\d+\)/)
 puts muls.sum { |s| s.scan(/\d+/).map(&:to_i).reduce(:*) }
+
+# Part 2
+instructions = input.scan(/(mul\(\d+,\d+\)|don\'t\(\)|do\(\))/).flatten
+enabled = true
+
+mult = instructions.reduce(0) do |acc, el|
+  val = 0
+  if el == "don't()"
+    enabled = false
+  elsif el == "do()"
+    enabled = true
+  elsif enabled
+    val = el.scan(/\d+/).map(&:to_i).reduce(:*)
+  end
+  acc + val
+end
+
+puts mult
