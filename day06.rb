401bc183e98ddbb5f273d918d5be086b0eb79417
Haseeb Qureshi <haseebqureshi@Haseebs-MacBook-Air.local>
2024-12-06 11:02:28 -0800

    Finish day 6

--- day06.rb
+++ day06.rb
@@ -20,7 +20,7 @@ def next_orientation(orientation)
   arr[arr.find_index(orientation) + 1]
 end
 
-orientation = ORIENTATIONS[0]
+orientation = ORIENTATIONS.first
 i, j = nil, nil
 GRID.each_index do |i2|
   GRID[0].each_index do |j2|
@@ -28,6 +28,7 @@ GRID.each_index do |i2|
   end
 end
 
+original_position = [i, j].dup
 GRID[i][j] = SEEN
 
 def next_position(orientation, i, j)
@@ -47,3 +48,42 @@ loop do
 end
 
 puts GRID.sum { |row| row.count { |el| el == SEEN } }
+
+# Part 2
+
+g = GRID.map(&:dup)
+
+sum = GRID.each_index.sum do |i|
+  GRID[i].each_index.count do |j|
+    next unless GRID[i][j] == SEEN # only place obstacles in places that are reachable
+    next if [i, j] == original_position
+
+    position = original_position.dup
+    orientation = ORIENTATIONS.first
+    key = "KEY-#{[i, j, orientation]}"
+
+    g[i][j] = OBSTACLE
+    loops_forever = false
+
+    until !in_grid?(g, *position)
+      i2, j2 = next_position(orientation, *position)
+      if !in_grid?(g, i2, j2)
+        break
+      elsif g[i2][j2] == OBSTACLE
+        orientation = next_orientation(orientation)
+        key = "KEY-#{[i, j, orientation]}"
+      elsif g[i2][j2] == key
+        loops_forever = true
+        break
+      else
+        g[i2][j2] = key
+        position = [i2, j2]
+      end
+    end
+
+    g[i][j] = EMPTY
+    loops_forever
+  end
+end
+
+puts sum
