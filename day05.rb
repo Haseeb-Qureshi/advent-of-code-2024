0909cfd7a3570f5c5bfcf1e2d8b946eba7129525
Haseeb Qureshi <haseebqureshi@Haseebs-MacBook-Air.local>
2024-12-05 16:20:42 -0800

    Finish day 5

--- day05.rb
+++ day05.rb
@@ -23,3 +23,23 @@ puts orderings.select { |l| valid?(l, predecessors) }
               .sum { |l| l[l.length / 2] }
 
 # Part 2
+
+invalids = orderings.reject { |l| valid?(l, predecessors) }
+
+def iterative_repair!(line, predecessors)
+  swap_first_error!(line, predecessors) while !valid?(line, predecessors)
+end
+
+def swap_first_error!(line, predecessors)
+  line.each_index do |i|
+    (i + 1...line.length).each do |j|
+      if predecessors[line[i]].include?(line[j])
+        line[i], line[j] = line[j], line[i]
+        return
+      end
+    end
+  end
+end
+
+invalids.each { |l| iterative_repair!(l, predecessors) }
+puts invalids.sum { |l| l[l.length / 2] }
