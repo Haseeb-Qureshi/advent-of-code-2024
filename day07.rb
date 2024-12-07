e3b361edb6e4e0de12545422efe9e9783afeae2b
Haseeb Qureshi <haseebqureshi@Haseebs-MacBook-Air.local>
2024-12-06 22:47:02 -0800

    Day 7 down

--- day07.rb
+++ day07.rb
@@ -11,17 +11,32 @@ input = File.readlines('data07.txt')
 #   - You recursively check every branch in this tree of possible continuations
 #   - It's extremely unlikely you'll revisit two of the same thing
 
-def has_combination?(target, sequence, symbols = [:+, :*])
-  suffix_combination?(target, sequence.drop(1), sequence.first, symbols)
+def has_combination?(target, sequence)
+  combinable?(target, sequence.drop(1), sequence.first)
 end
 
-def suffix_combination?(target, suffix, current, symbols)
-  return true if current == target
-  return false if suffix.empty?
+def combinable?(target, suffix, current, symbols = [:+, :*])
+  return current == target if suffix.empty?
 
   symbols.any? do |sym|
-    suffix_combination?(target, suffix.drop(1), current.send(sym, suffix.first), symbols)
+    combinable?(target, suffix.drop(1), current.send(sym, suffix.first), symbols)
   end
 end
 
 puts input.sum { |(sum, seq)| has_combination?(sum, seq) ? sum : 0 }
+
+# Part 2
+
+class Integer
+  def append(n)
+    (self.to_s + n.to_s).to_i
+  end
+end
+
+def has_combination_with_append?(target, sequence)
+  syms = [:+, :*, :append]
+  combinable?(target, sequence.drop(1), sequence.first, syms) ||
+    combinable?(target, sequence.drop(2), sequence[0].append(sequence[1]), syms)
+end
+
+puts input.sum { |(sum, seq)| has_combination_with_append?(sum, seq) ? sum : 0 }
