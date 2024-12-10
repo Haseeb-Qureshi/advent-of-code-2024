cdc07a71df664203fb82711a85c190df2c720297
Haseeb Qureshi <haseebqureshi@Haseebs-MacBook-Air.local>
2024-12-09 23:11:09 -0800

    Complete day 10

--- day10.rb
+++ day10.rb
@@ -3,49 +3,33 @@
 require_relative 'helpers'
 grid = File.readlines('data10.txt').map { |l| l.chomp.chars.map(&:to_i) }
 
-# Find each 9 and follow every path from there that climbs up
-can_reach_nines = Array.new(grid.length) { Array.new(grid[0].length) { 0 } }
+reachable_9s = Array.new(grid.length) { Array.new(grid[0].length) { 0 } }
 
-def neighbors(i, j, grid)
-  [
-    [i + 1, j],
-    [i - 1, j],
-    [i, j + 1],
-    [i, j - 1],
-  ].select { |x, y| in_grid?(grid, x, y) }
-end
-
-def mark_trails_reachable_from!(orig_i, orig_j, grid, can_reach_nines)
+def mark_zeroes_reachable_from!(orig_i, orig_j, grid, reachable_9s)
   queue = [[orig_i, orig_j]]
   seen = Set.new([[orig_i, orig_j]])
+  zeroes = Set.new
 
   until queue.empty?
     i, j = queue.pop
-
-
-
+    reachable_9s[i][j] += 1
+    zeroes << [i, j] if grid[i][j] == 0
+    neighbors(i, j, grid).select { |i2, j2| grid[i2][j2] == grid[i][j] - 1 }.each { |coord| queue << coord }
   end
+  zeroes.count
 end
 
-def nines_reachable_from(orig_i, orig_j, grid)
-  queue = [[orig_i, orig_j]]
-  seen = Set.new([[orig_i, orig_j]])
-  nines = Set.new
-
-  until queue.empty?
-    i, j = queue.pop
-    nines << [i, j] if grid[i][j] == 9
-    neighbors(i, j, grid).select { |i2, j2| grid[i2][j2] == grid[i][j] + 1 }.each { |coord| queue << coord }
-  end
-  nines.count
-end
-
+# Output how many 9s are reachable from a 0
 sum = grid.each_with_index.sum do |row, i|
   row.each_with_index.sum do |n, j|
-    next 0 unless n == 0
-    # mark_trails_from!(i, j, grid, can_reach_nines)
-    nines_reachable_from(i, j, grid)
+    next 0 unless n == 9
+    mark_zeroes_reachable_from!(i, j, grid, reachable_9s)
   end
 end
 
 puts sum
+
+# Part 2
+
+# Output how many possible paths from a 9 to a 0
+puts grid.flatten.zip(reachable_9s.flatten).select { |val, count| val == 0 }.sum(&:last)
--- helpers.rb
+++ helpers.rb
@@ -1,3 +1,12 @@
 def in_grid?(grid, i, j)
   (0...grid.length).include?(i) && (0...grid[0].length).include?(j)
 end
+
+def neighbors(i, j, grid)
+  [
+    [i + 1, j],
+    [i - 1, j],
+    [i, j + 1],
+    [i, j - 1],
+  ].select { |i2, j2| in_grid?(grid, i2, j2) }
+end
