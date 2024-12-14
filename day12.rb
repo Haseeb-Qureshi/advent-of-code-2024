72c0b9366fd460157213a0e702adbdd42f1f4ccb
Haseeb Qureshi <haseebqureshi@Haseebs-MacBook-Air.local>
2024-12-14 16:30:55 -0500

    Partial work on 12, still haven't solved

--- day12.rb
+++ day12.rb
@@ -35,6 +35,7 @@ end
 def area_from(i, j)
   char = GRID[i][j]
   queue = [[i, j]]
+  span = [[i, j]]
   area = 0
   until queue.empty?
     i2, j2 = queue.pop
@@ -44,10 +45,11 @@ def area_from(i, j)
     # Ok we're the same character!
 
     VISITED[i2][j2] = true
+    span << [i2, j2]
     area += 1
     queue.concat(neighbors(i2, j2, GRID))
   end
-  area
+  [area, span]
 end
 
 def bordering_characters(i, j, char)
@@ -58,6 +60,7 @@ def perimeter_from(i, j)
   char = GRID[i][j]
   queue = [[i, j]]
   perimeter = 0
+  perimeter_path = Set.new
   seen_in_perimeter = Set.new
   until queue.empty?
     i2, j2 = queue.pop
@@ -69,25 +72,75 @@ def perimeter_from(i, j)
 
     if bordering_characters(i2, j2, char) > 0
       # Ok we're a valid perimeter!
+      perimeter_path << [i2, j2]
       perimeter += bordering_characters(i2, j2, char)
     end
     queue.concat(neighbors(i2, j2, GRID))
   end
-  # p seen_in_perimeter
-  # seen_in_perimeter.each { |i, j| GRID[i][j] = 'P'.blue }
-  # puts GRID.map(&:join)
-  # p ["Perimeter: ", char, perimeter]
-  perimeter
+
+  [perimeter, perimeter_path]
 end
 
 total_price = 0
 GRID.each_index do |i|
   GRID[0].each_index do |j|
     next if VISITED[i][j] || GRID[i][j] == WALL
-    area = area_from(i, j)
-    perimeter = perimeter_from(i, j)
+    area, _ = area_from(i, j)
+    perimeter, _ = perimeter_from(i, j)
     total_price += area * perimeter
   end
 end
 
 puts total_price
+
+# Part 2
+
+def sides_from(i, j)
+  external_sides_from(i, j) + internal_sides_from(i, j)
+end
+
+def external_sides_from(i, j)
+  _, perimeter_path = perimeter_from(i, j)
+  traced = perimeter_path.zip([false].cycle).to_h
+
+  start = [i, j]
+  perimeter_path.delete(start)
+  traced[start] = true
+  direction = nil
+
+  until perimeter_path.empty?
+    # add the number of neighbors, that = sides
+    # If you are going in the same direction, skip?
+  end
+  # Start at one node along the perimeter
+  # Cases:
+  #   Extend your hand to the left, how many corners do you feel?
+  #   Each corner denotes a side
+  #   How do you detect a corner? A corner is two adjacent squares are not part of the same structure (up & left), (down & left), (down & right), (up & right)
+  #   If all 4 are not part of the same structure, you add 4 sides
+  #   If 3 are not part of the same structure, you add 2 sides ??
+  #   If 2 ADJACENTS are not part of the same structure BUT NO DIAG, you add 1 side
+  #   If 2 ADJACENTS are not part of the same structure BUT YES TO DIAG, you add 2 sides
+  #   If 1 is not part of the same structure, you add nothing
+  #   F
+  #   --
+  #   FF
+  #   --
+  #   FFFF
+  #   FFF
+  #   --
+  #   F
+  #   FFFFF
+  #    F
+  #   --
+  #   FF   3 + 1  4
+  #    FF  1 + 1  6
+  #     F  3      9 # Nope, my algorithm doesn't work. Think about this some more. Use pen and paper.
+  # What if there's a single jutting out peninsula?
+  # What if there's a ladder moving left and down?
+end
+
+# First, find the perimeter and return the perimeter path
+# BFS the perimeter and count every time you change directions—that's a new side
+# Then find internal sides
+  #
