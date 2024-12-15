ee3b1bca7c4cac201fe5c2a229cf2572b84b44b2
Haseeb Qureshi <haseebqureshi@Haseebs-MacBook-Air.local>
2024-12-15 09:10:40 -0800

    Complete day 13

--- day13.rb
+++ day13.rb
@@ -7,25 +7,33 @@ buttons = input.each_slice(4).reduce([]) do |acc, (a, b, p, _)|
   end
 end
 
-tokens = 0
-buttons.each do |(ax, ay, bx, by, px, py)|
-  # Solve the system of equations
-  lcm = ax.lcm(ay)
-  bx2 = bx * lcm / ax
-  px2 = px * lcm / ax
-  by2 = by * lcm / ay
-  py2 = py * lcm / ay
-  right_side = px2 - py2
-  left_side = bx2 - by2
-  b = right_side.fdiv(left_side)
-  a = (px - bx * b).fdiv(ax)
+def compute_tokens(buttons, offset = 0)
+  buttons.sum do |(ax, ay, bx, by, px, py)|
+    # Solve the system of equations
+    lcm = ax.lcm(ay)
+    bx2 = bx * lcm / ax
+    px2 = (px + offset) * lcm / ax
+    by2 = by * lcm / ay
+    py2 = (py + offset) * lcm / ay
+    right_side = px2 - py2
+    left_side = bx2 - by2
+    b = right_side.fdiv(left_side)
+    a = (px + offset - bx * b).fdiv(ax)
 
-  # See if there is a whole number & positive solution
-  next if a % 1 > 0 || b % 1 > 0
-  next if a < 0 || b < 0
-
-  # Add the tokens
-  tokens += a * 3 + b
+    # See if there is a whole number & positive solution
+    if a % 1 > 0 || b % 1 > 0
+      0
+    elsif a < 0 || b < 0
+      0
+    else
+      # Add the tokens
+      (a * 3 + b).to_i
+    end
+  end
 end
 
-puts tokens
+puts compute_tokens(buttons)
+
+# Part 2
+
+puts compute_tokens(buttons, 10_000_000_000_000)
