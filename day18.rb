b1e77e71cf43087c277d3dd64a473c468deede4a
Haseeb Qureshi <haseebqureshi@Haseebs-MacBook-Air.local>
2024-12-17 23:17:50 -0800

    Finish day 18 part 2

--- day18.rb
+++ day18.rb
@@ -1,52 +1,56 @@
 # Part 1
 require_relative 'helpers'
-require 'pqueue'
 
 SAFE = '.'.freeze
 CORRUPTED = '#'.freeze
 
 GRID_SIZE = 70 + 1
-file = File.readlines('data18.txt')
-# file = '5,4
-# 4,2
-# 4,5
-# 3,0
-# 2,1
-# 6,3
-# 2,4
-# 1,5
-# 0,6
-# 3,3
-# 2,6
-# 5,1'.lines
-coords = file.first(1024).map { |l| l.chomp.split(',').map(&:to_i).reverse }
-
+FILE = File.readlines('data18.txt')
 GRID = Array.new(GRID_SIZE) { Array.new(GRID_SIZE) { SAFE } }
-coords.each  { |i, j| GRID[i][j] = CORRUPTED }
+
+def setup_grid!(first)
+  coords = FILE.first(first).map { |l| l.chomp.split(',').map(&:to_i).reverse }
+  GRID.each_index { |i| GRID[0].each_index { |j| GRID[i][j] = SAFE } }
+  coords.each  { |i, j| GRID[i][j] = CORRUPTED }
+end
 
 def bfs_from(start_i, start_j)
   seen = Set.new
-  queue = PQueue.new { |a, b| a.last < b.last }
+  queue = [[start_i, start_j, 0]]
 
-  queue << [start_i, start_j, Set.new([[start_i, start_j]]), 1]
   until queue.empty?
-    i, j, path, cost = queue.pop
+    i, j, dist = queue.shift
     next if seen.include?([i, j])
     seen << [i, j]
     next if GRID[i][j] == CORRUPTED
 
-    # viz_path(path)
-    return cost  - 1 if i == GRID.length - 1 && j == GRID[0].length - 1
+    return dist if i == GRID.length - 1 && j == GRID[0].length - 1
     # add neighbors
-    neighbors(i, j, GRID).each { |i2, j2| queue << [i2, j2, path + [[i2, j2]], cost + 1]}
+    neighbors(i, j, GRID).each { |i2, j2| queue << [i2, j2, dist + 1] }
+  end
+  nil
+end
+
+def run_bfs!(first)
+  setup_grid!(first)
+  bfs_from(0, 0)
+end
+
+puts run_bfs!(1024)
+
+# Part 2
+def binary_search(lower = 0, upper = FILE.length - 1)
+  mid = (upper - lower) / 2 + lower
+  if lower == upper # terminate here
+    return lower - 1 if run_bfs!(lower).nil? && run_bfs!(lower - 1)
+  end
+
+  # if midpoint fails to bfs, go right
+  if run_bfs!(mid).nil?
+    binary_search(lower, mid)
+  else
+    binary_search(mid + 1, upper)
   end
 end
 
-def viz_path(path)
-  path.each { |i, j| GRID[i][j] = 'O' }
-  puts GRID.map(&:join)
-  path.each { |i, j| GRID[i][j] = SAFE }
-  puts
-end
-
-puts bfs_from(0, 0)
+puts FILE[binary_search]
